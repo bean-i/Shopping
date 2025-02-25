d9a4e64b96513ec247a3f5ffe21130d60190740a
been <leebin0603@naver.com>
2025-02-26 01:26:34 +0900

    Feat: 메인 화면 구현

---
+++ Shopping/Presentation/ShoppingMain/ShoppingMainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ShoppingMainViewController.swift
+//  CodeBaseAutoLayout
+//
+//  Created by 이빈 on 1/15/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class ShoppingMainViewController: BaseViewController<ShoppingMainView> {
+    
+    let viewModel = ShoppingMainViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func bind() {
+        let input = ShoppingMainViewModel.Input(searchText: mainView.searchBar.rx.searchButtonClicked
+            .withLatestFrom(mainView.searchBar.searchTextField.rx.text.orEmpty))
+        let output = viewModel.transform(input: input)
+        
+        // 타이틀
+        output.title
+            .bind(to: navigationItem.rx.title)
+            .disposed(by: disposeBag)
+
+        // alert
+        output.searchFail
+            .subscribe(with: self) { owner, _ in
+                owner.showAlert(title: "두 글자 이상 입력",
+                                message: "검색어를 두 글자 이상 입력해 주세요.",
+                                button: "확인")
+            } onDisposed: { owner in
+                print("output.searchFail onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        // 화면 전환
+        output.searchSuccess
+            .subscribe(with: self) { owner, value in
+                let vc = ShoppingSearchResultViewController()
+                vc.viewModel.title.accept(value)
+                owner.navigationController?.pushViewController(vc, animated: true)
+            } onDisposed: { owner in
+                print("output.searchSuccess onDisposed")
+            }
+            .disposed(by: disposeBag)
+    }
+}
---
+++ Shopping/Presentation/ShoppingMain/ShoppingMainViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  ShoppingMainViewModel.swift
+//  CodeBaseAutoLayout
+//
+//  Created by 이빈 on 2/6/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ShoppingMainViewModel: BaseViewModel {
+    
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        let searchText: Observable<String>
+    }
+    
+    struct Output {
+        let title: Observable<String> = Observable.just("도봉러의 쇼핑쇼핑")
+        let searchFail: PublishSubject<Bool>
+        let searchSuccess: PublishSubject<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let searchFail = PublishSubject<Bool>()
+        let searchSuccess = PublishSubject<String>()
+        
+        
+        input.searchText
+//            .map { $0.count >= 2 }
+            .subscribe(with: self) { owner, value in
+//                if bool {
+//                    searchSuccess.onNext(true)
+//                } else {
+//                    searchFail.onNext(true)
+//                }
+                if value.count >= 2 {
+                    searchSuccess.onNext(value)
+                } else {
+                    searchFail.onNext(true)
+                }
+            } onDisposed: { owner in
+                print("input.searchText onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            searchFail: searchFail,
+            searchSuccess: searchSuccess
+        )
+    }
+}
