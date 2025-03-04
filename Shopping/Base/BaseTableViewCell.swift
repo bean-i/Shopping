0e26a8c8b2a034b346b285a12a7b0b3ae09ad2a7
been <leebin0603@naver.com>
2025-03-04 23:03:44 +0900

    Feat: BaseTableViewCell 생성

---
+++ Shopping/Base/BaseTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseTableViewCell.swift
+//  Shopping
+//
+//  Created by 이빈 on 3/4/25.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+    
+}
